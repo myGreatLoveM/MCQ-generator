c391aeae0f8e076e4149d7583a79572c05d2c379
prem <praulayar994@gmail.com>
2023-12-11 22:46:04 +0530

    initial project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='prem',
+    author_email='praulayar994@gmail.com',
+    install_requires=[],
+    packages=find_packages()
+)
